910542fe4885d6a6bd85fd8e2b41709647b7ca3b
Devon-Peroutky <devonperoutky@vividcortex.com>
2015-06-16 17:19:18 -0400

    Email Server - V1.0

--- app/js/logic/app.js
+++ app/js/logic/app.js
@@ -7,7 +7,7 @@ twjApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl : 'partials/home.html',
-            controller  : 'tempCtrl'
+            controller  : 'formCtrl'
         })
         .when('/blog', {
             templateUrl : 'partials/blog.html',
@@ -29,9 +29,34 @@ twjApp.controller('postCtrl', ['$scope', '$routeParams', 'Posts', function($scop
     });
 }]);
 
-twjApp.controller('tempCtrl', ['$scope', function($scope) {
-    console.log("RIGHT CONTROLLER");
-    $scope.message = 'Sup Bitch';
+twjApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.button = "Send Message";
+    $scope.formData = {};
+
+    // process the form
+    $scope.processForm = function() {
+        $scope.button = "Sending...";
+        console.log($scope.formData.name + ", " + $scope.formData.email  + ", " + $scope.formData.phone  + ", " + $scope.formData.text);
+        $http({
+            url: "/send", 
+            method: "GET",
+            params: {
+                name: $scope.formData.name,
+                email: $scope.formData.email,
+                phone: $scope.formData.phone,
+                message: $scope.formData.text
+            }
+        })
+        .success(function(data) {
+            console.log("Succefully returned Data: " + data);
+            $scope.button = "Sent!";
+        })
+        .error(function(data){
+            console.log("Error sending the email: " + data);
+            $scope.message = "Error sending the message. Email us directly at trippinwithjamie@gmail.com";
+            $scope.button = "Error!";
+        });
+    };
 }]);
 
 twjApp.controller('blogCtrl', ['$scope', 'Posts', function($scope, Posts) {
--- server.js
+++ server.js
@@ -6,6 +6,24 @@ var app      = express();                               // create our app w/ exp
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var nodemailer = require("nodemailer");
+
+// email =========================
+var smtpTransport = nodemailer.createTransport("SMTP",{
+	service: "Gmail",
+	auth: {
+		user: "trippinwithjamie@gmail.com",
+		pass: "trip1345pin"
+	},
+});
+
+var buildMessage = function(name, email, phone, text) {
+	var query = "Name: " + name + "\n" +
+				"Email: " + email + "\n" +
+				"Phone: " + phone + "\n" +
+				"Message: " + text + "\n";
+	return query;
+}
 
 // configuration =================
 console.log(__dirname);
@@ -22,6 +40,24 @@ app.get('/', function(req, res) {
     res.sendfile('app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+app.get('/send', function(req, res) {
+	var mailOptions={
+		to : "devonperoutky@gmail.com",
+		subject : "Travel Inquiry from " + req.query.name,
+		text : buildMessage(req.query.name, req.query.email, req.query.phone, req.query.message)
+	}
+
+	smtpTransport.sendMail(mailOptions, function(error, response){
+		if(error){
+			console.log(error);
+			res.status(500).send('Error sending the message');
+		} else {
+			console.log("Message sent: " + response.message);
+			res.end("sent");
+		}
+	});
+});
+
 // Error Handling  ==================================
 app.get('*', function(req, res, next) {
 	console.log("404 error!");
@@ -32,7 +68,7 @@ app.get('*', function(req, res, next) {
 
 // handling 404 errors
 app.use(function(err, req, res, next) {
-	console.log("USE");
+	console.log("Handling a 404 error");
 	if(err.status !== 404) {
 		return next();
 	}
